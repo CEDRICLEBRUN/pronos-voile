4bf55d8ad5717eabe9171cdfc35bdfd5013ba6cc
cedric le brun <lebruncedric8@gmail.com>
2023-10-10 14:07:55 +0200

    Model Result

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_115955) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_120636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_115955) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "results", force: :cascade do |t|
+    t.integer "position"
+    t.bigint "boat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_results_on_boat_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -116,4 +124,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_115955) do
   add_foreign_key "bets", "users"
   add_foreign_key "boats", "races"
   add_foreign_key "crews", "users"
+  add_foreign_key "results", "boats"
 end
